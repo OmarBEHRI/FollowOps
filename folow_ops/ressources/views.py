ffa530b9d94742b8334192a8533e9a786c6d508d
Ouiam Erreyyadi <erreyyadiouiam1@gmail.com>
2025-07-16 14:35:07 +0200

    Views

--- folow_ops/ressources/views.py
+++ folow_ops/ressources/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Ressource
 
 # Create your views here.
+def ressources(request):
+    ressources = Ressource.objects.all()
+    return render(request, 'ressources/ressources.html', {'ressources': ressources})
+
+def ressourcesDetails(request, pk):
+    ressource = Ressource.objects.get(pk=pk)
+    return render(request, 'ressources/ressourcesDetails.html', {'ressource': ressource})
--- folow_ops/tickets/views.py
+++ folow_ops/tickets/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Ticket
 
+# Create your views here.
 def tickets(request):
-    return render(request, 'tickets.html')
+    tickets = Ticket.objects.all()
+    return render(request, 'tickets/tickets.html', {'tickets': tickets})
+
+def ticketDetails(request, pk):
+    ticket = Ticket.objects.get(pk=pk)
+    return render(request, 'tickets/ticketDetails.html', {'ticket': ticket})
