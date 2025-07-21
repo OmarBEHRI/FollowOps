5bf0cd3010bb4cb81e398dd2cdc8ed7ba3f2050f
OmarBEHRI <omarbehri1@gmail.com>
2025-07-21 16:25:16 +0100

    Deleted Typo folder

--- folow_ops/projects/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.projects, name='projects'),
-    path('<pk>/', views.projectDetails, name='projectDetails'),
-]
--- folow_ops/projects/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def projects(request):
-    return render(request, 'projects.html')
-
-
-def projectDetails(request, pk):
-    return render(request, 'projectDetails.html', {'pk': pk})
-
